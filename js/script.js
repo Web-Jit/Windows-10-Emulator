d765de3c0457f56a855cbb57af04f44eee218e21
Nabjeet Kumar <93638460+Web-Jit@users.noreply.github.com>
2021-12-10 20:32:25 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -259,55 +259,10 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
-//  CPU-Z 
-
-//Make the DIV element draggagle:
-dragElement(document.getElementById("cpuz"));
-
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header1")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id + "header1").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
 //  GPU-Z 
 
 //Make the DIV element draggagle:
-dragElement(document.getElementById("gpuz"));
+dragElement(document.getElementById("teams"));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
@@ -412,4 +367,11 @@ $(document).ready(function(){
 // }
 
 // showTime();
+
+$(window).on('load', function() { // makes sure the whole site is loaded 
+  $('#status').fadeOut(); // will first fade out the loading animation 
+  $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+  $('body').delay(350).css({'overflow':'hidden'});
+})
+
 
